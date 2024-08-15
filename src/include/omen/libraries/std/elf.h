7c8fe62341333f3f1097c29e4bc8cdb5e1dc3b60
Shobhit <shobhit32t@gmail.com>
2024-08-16 00:24:47 +0530

    Fixed formatting to improve readability

--- src/include/omen/libraries/std/elf.h
+++ src/include/omen/libraries/std/elf.h
@@ -3,16 +3,16 @@
  */
 #include <omen/libraries/std/stdint.h>
 
-typedef uint64_t    Elf64_Addr;
-typedef uint64_t    Elf64_Off;
-typedef uint16_t    Elf64_Section;
-typedef uint16_t    Elf64_Versym;
+typedef uint64_t      Elf64_Addr;
+typedef uint64_t      Elf64_Off;
+typedef uint16_t      Elf64_Section;
+typedef uint16_t      Elf64_Versym;
 typedef unsigned char Elf_Byte;
-typedef uint16_t    Elf64_Half;
-typedef int32_t     Elf64_Sword;
-typedef uint32_t    Elf64_Word;
-typedef int64_t     Elf64_Sxword;
-typedef uint64_t    Elf64_Xword;
+typedef uint16_t      Elf64_Half;
+typedef int32_t       Elf64_Sword;
+typedef uint32_t      Elf64_Word;
+typedef int64_t       Elf64_Sxword;
+typedef uint64_t      Elf64_Xword;
 
 #define EI_NIDENT 16
 
@@ -36,28 +36,28 @@ typedef struct {
 
 // structure of Program header
 typedef struct {
-    uint32_t p_type;
-    uint32_t p_flags;
-    Elf64_Off p_offset;
+    uint32_t   p_type;
+    uint32_t   p_flags;
+    Elf64_Off  p_offset;
     Elf64_Addr p_vaddr;
     Elf64_Addr p_paddr;
-    uint64_t p_filesz;
-    uint64_t p_memsz;
-    uint64_t p_align;
+    uint64_t   p_filesz;
+    uint64_t   p_memsz;
+    uint64_t   p_align;
 } Elf64_Phdr;
 
 // structure of Section header
 typedef struct {
-    uint32_t sh_name;
-    uint32_t sh_type;
-    uint64_t sh_flags;
+    uint32_t   sh_name;
+    uint32_t   sh_type;
+    uint64_t   sh_flags;
     Elf64_Addr sh_addr;
-    Elf64_Off sh_offset;
-    uint64_t sh_size;
-    uint32_t sh_link;
-    uint32_t sh_info;
-    uint64_t sh_addralign;
-    uint64_t sh_entsize;
+    Elf64_Off  sh_offset;
+    uint64_t   sh_size;
+    uint32_t   sh_link;
+    uint32_t   sh_info;
+    uint64_t   sh_addralign;
+    uint64_t   sh_entsize;
 } Elf64_Shdr;
 
 #endif
