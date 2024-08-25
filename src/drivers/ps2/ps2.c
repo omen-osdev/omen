be2239fb1502698e86a56c8cbd8677eaa9ccc050
toadkarter <vrakhmanin1@gmail.com>
2024-08-25 18:15:11 +0100

    Adds initial empty interface for PS/2 driver to begin work from.

---
+++ src/drivers/ps2/ps2.c
@@ -0,0 +1,25 @@
+#include "ps2.h"
+
+char* create_ps2_dd()
+{
+    return NULL;
+}
+
+status_t init_ps2_dd()
+{
+    return -1;
+}
+uint64_t ps2_dd_ioctl(uint64_t id, uint32_t request, void* data)
+{
+    return -1;
+}
+
+uint64_t ps2_dd_rcvd()
+{
+    return -1;
+}
+
+uint64_t ps2_dd_read(uint64_t id, uint8_t* buffer)
+{
+    return -1;
+}
