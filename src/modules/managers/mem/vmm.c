edf6ac6bae3b7e340d54409b14d21f7c5d10369a
Shobhit <shobhit32t@gmail.com>
2024-08-25 23:20:20 +0530

    Fixed incorrect declare types

--- src/modules/managers/mem/vmm.c
+++ src/modules/managers/mem/vmm.c
@@ -1,4 +1,4 @@
-#include <omen/managers/mem/vmm/paging_structs.h>
+#include <omen/managers/mem/vmm.h>
 #include <omen/managers/mem/pmm.h>
 #include <omen/libraries/std/stdint.h>
 
@@ -17,16 +17,16 @@ void* virt_to_phys(uint64_t virt_addr, pdTable *pml4) {
     pageMapIndex idxMap;
     page_to_map(virt_addr, &idxMap);
 
-    pml4_entry pml4_e =  pml4->entry[idxMap.PML4_idx];
+    pd_entry pml4_e = pml4->entry[idxMap.PML4_idx];
     pdTable *pdpt = NULL;
     if((pml4_e & PDE_PRESENT) == 0b01) {
-        pdpt = (pdptTable*)pml4_e; // check
+        pdpt = (pdTable*)pml4_e; // check
     }
     else {
         return NULL;
     }
 
-    pdpt_entry pdpt_e = pdpt->entry[idxMap.PDPT_idx];
+    pd_entry pdpt_e = pdpt->entry[idxMap.PDPT_idx];
     pdTable *pd = NULL;
     if( (pdpt_e & PDE_PRESENT) == 0b01) {
         pd = (pdTable*)pdpt_e;
@@ -54,13 +54,13 @@ void* virt_to_phys(uint64_t virt_addr, pdTable *pml4) {
     }
 }
 
-page_directory* get_pml4() {
+pdTable* get_pml4() {
     struct page_directory* pml4;
     __asm__("movq %%cr3, %0" : "=r"(pml4));
     return TO_KERNEL_MAP(pml4);
 }
 
-void set_pml4(struct page_directory* pml4) {
+void set_pml4(pdTable* pml4) {
     __asm__("movq %0, %%cr3" : : "r" (FROM_KERNEL_MAP(pml4)));
 }
 
